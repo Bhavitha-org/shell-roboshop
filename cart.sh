81b3072c980e73c491dc76e693ca899d9886370f
Bhavitha-Lakkireddy <bhavii342@gmail.com>
2026-02-06 16:16:01 +0530

    configuration

---
+++ cart.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="/var/log/shell-roboshop/$0.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SCRIPT_DIR=$PWD   #doubt
+MONGODB_HOST="mongodb.exploreops.online"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE() {
+if [ $1 -ne 0 ]; then
+    echo -e "$2 ...$R FAILURE $N" | tee -a $LOGS_FILE  
+    exit 1
+else 
+    echo -e "$2 ...$G SUCCESS $N" | tee -a $LOGS_FILE      
+fi
+}
+
+dnf module disable nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Disabling nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
+VALIDATE $? "Enabling nodejs"
+
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Installing nodejs"
+
+id roboshop &>>$LOGS_FILE   #echo $? return 0, if roboshop exists, else greater than 0
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "Creating system user"
+else
+    echo -e "roboshop user already exist...$Y SKIPPING $N"
+fi
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$LOGS_FILE
+VALIDATE $? "Downloading cart code"
+
+cd /app
+VALIDATE $? "Moving to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/cart.zip &>>$LOGS_FILE
+VALIDATE $? "Unzipping cart code"
+
+npm install &>>$LOGS_FILE
+VALIDATE $? "Installing dependencies"
+
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
+VALIDATE $? "Created systemctl service"
+
+systemctl daemon-reload
+systemctl enable cart &>>$LOGS_FILE
+systemctl start cart
+VALIDATE $? "Starting and enabling cart"
+
