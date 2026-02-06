f6827f552a0a2e14dce95cfcd0e02154b326e794
Bhavitha-Lakkireddy <bhavii342@gmail.com>
2026-02-06 16:09:24 +0530

    configuration

--- user.sh
+++ user.sh
@@ -1,86 +1,71 @@
 #!/bin/bash
 
-# USERID=$(id -u)
-# LOGS_FOLDER="/var/log/shell-roboshop"
-# LOGS_FILE="/var/log/shell-roboshop/$0.log"
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="/var/log/shell-roboshop/$0.log"
 
-# R="\e[31m"
-# G="\e[32m"
-# Y="\e[33m"
-# N="\e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-# SCRIPT_DIR=$PWD   #doubt
-# MONGODB_HOST="mongodb.exploreops.online"
+SCRIPT_DIR=$PWD   #doubt
+MONGODB_HOST="mongodb.exploreops.online"
 
-# if [ $USERID -ne 0 ]; then
-#     echo -e "$R please run this script with root user access $N" | tee -a $LOGS_FILE
-#     exit 1
-# fi
+if [ $USERID -ne 0 ]; then
+    echo -e "$R please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
 
-# mkdir -p $LOGS_FOLDER
+mkdir -p $LOGS_FOLDER
 
-# VALIDATE() {
-# if [ $1 -ne 0 ]; then
-#     echo -e "$2 ...$R FAILURE $N" | tee -a $LOGS_FILE  
-#     exit 1
-# else 
-#     echo -e "$2 ...$G SUCCESS $N" | tee -a $LOGS_FILE      
-# fi
-# }
+VALIDATE() {
+if [ $1 -ne 0 ]; then
+    echo -e "$2 ...$R FAILURE $N" | tee -a $LOGS_FILE  
+    exit 1
+else 
+    echo -e "$2 ...$G SUCCESS $N" | tee -a $LOGS_FILE      
+fi
+}
 
-# dnf module disable nodejs -y &>>$LOGS_FILE
-# VALIDATE $? "Disabling nodejs"
+dnf module disable nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Disabling nodejs"
 
-# dnf module enable nodejs:20 -y &>>$LOGS_FILE
-# VALIDATE $? "Enabling nodejs"
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
+VALIDATE $? "Enabling nodejs"
 
-# dnf install nodejs -y &>>$LOGS_FILE
-# VALIDATE $? "Installing nodejs"
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Installing nodejs"
 
-# id roboshop &>>$LOGS_FILE   #echo $? return 0, if roboshop exists, else greater than 0
-# if [ $? -ne 0 ]; then
-#     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
-#     VALIDATE $? "Creating system user"
-# else
-#     echo -e "roboshop user already exist...$Y SKIPPING $N"
-# fi
-# mkdir -p /app 
-# VALIDATE $? "Creating app directory"
+id roboshop &>>$LOGS_FILE   #echo $? return 0, if roboshop exists, else greater than 0
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "Creating system user"
+else
+    echo -e "roboshop user already exist...$Y SKIPPING $N"
+fi
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
 
-# curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOGS_FILE
-# VALIDATE $? "Downloading catalogue code"
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOGS_FILE
+VALIDATE $? "Downloading user code"
 
-# cd /app
-# VALIDATE $? "Moving to app directory"
+cd /app
+VALIDATE $? "Moving to app directory"
 
-# rm -rf /app/*
-# VALIDATE $? "Removing existing code"
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
 
-# unzip /tmp/catalogue.zip &>>$LOGS_FILE
-# VALIDATE $? "Unzipping catalogue code"
+unzip /tmp/user.zip &>>$LOGS_FILE
+VALIDATE $? "Unzipping user code"
 
-# npm install &>>$LOGS_FILE
-# VALIDATE $? "Installing dependencies"
+npm install &>>$LOGS_FILE
+VALIDATE $? "Installing dependencies"
 
-# cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
-# VALIDATE $? "Created systemctl service"
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
+VALIDATE $? "Created systemctl service"
 
-# systemctl daemon-reload
-# systemctl enable catalogue &>>$LOGS_FILE
-# systemctl start catalogue
-# VALIDATE $? "Starting and enabling catalogue"
-
-# cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
-# dnf install mongodb-mongosh -y
-
-# INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')   #doubt
-                                
-# if [ $INDEX -le 0 ]; then
-#     mongosh --host $MONGODB_HOST </app/db/master-data.js
-#     VALIDATE $? "Loading products"
-# else
-#     echo -e "Products already loaded ... $Y SKIPPING $N"
-# fi
-
-# systemctl restart catalogue
-# VALIDATE $? "Restarting catalogue"
+systemctl daemon-reload
+systemctl enable user &>>$LOGS_FILE
+systemctl start user
+VALIDATE $? "Starting and enabling user"
