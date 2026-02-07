9e72e53ca086acee1c30be4d956fa23e0c9b2474
Bhavitha <lbhavitha342@gmail.com>
2026-02-07 22:18:28 +0530

    config

---
+++ rabbitmq.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="/var/log/shell-roboshop/$0.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SCRIPT_DIR=$PWD   #doubt
+MYSQL_HOST="mysql.exploreops.online"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE() {
+if [ $1 -ne 0 ]; then
+    echo -e "$2 ...$R FAILURE $N" | tee -a $LOGS_FILE  
+    exit 1
+else 
+    echo -e "$2 ...$G SUCCESS $N" | tee -a $LOGS_FILE      
+fi
+}
+
